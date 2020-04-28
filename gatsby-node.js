7e13506bc33c6f1d07691bb1f71d689c6554d803
Conor Campbell <conorc1000@gmail.com>
2020-04-28 13:48:45 +1000

    setting up login route

---
+++ gatsby-node.js
@@ -0,0 +1,5 @@
+exports.onCreatePage = ({ page, actions}) => {
+    if (page.path.match(/^\/dashboard/)) {
+        page.matchPath = '/dashboard/*'
+    }
+}
---
+++ src/components/route-base.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RouteBase = () => 
+    <h1>All you bases belong to us</h1>
+
+
+export default RouteBase;
