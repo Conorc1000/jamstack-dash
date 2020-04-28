0cd2aec0e37b5cda0e14f5e6ed1007edce100894
Conor Campbell <conorc1000@gmail.com>
2020-04-28 17:21:32 +1000

    authentication

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { identityContextProvider } from 'react-netlify-identity-widget';
+import { IdentityContextProvider } from 'react-netlify-identity-widget';
 import './layout.css';
 
 const Layout = ({ children }) => (
-    <identityContextProvider url="https://jamstack-auth-setup.netlify.com">
+    <IdentityContextProvider url="https://jamstack-auth-setup.netlify.com">
         <header>
             <Link to="/">Jamstack App</Link>
         </header>
         <main>{children}</main>
-    </identityContextProvider>
+    </IdentityContextProvider>
 )
 
 export default Layout
